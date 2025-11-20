81ed81f7eb6276fd73740c499242add5e8f16a97
Huzaifa Faizan <huzaifafaizan@Huzaifas-MacBook-Air.local>
2025-11-20 13:55:28 +1100

    Updated navbar logo + CSS2

--- components/About.tsx
+++ components/About.tsx
@@ -1,7 +1,6 @@
 // components/About.tsx
 export default function About() {
     return (
-        //hello
       <section id="about" className="section">
         <div className="container section__header">
           <h2>About Target Bluprint</h2>
